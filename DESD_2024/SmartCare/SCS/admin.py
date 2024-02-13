63c9542af9bf935f2f1accf737dccbcbdece0521
Tim <timkchapman@gmail.com>
2024-02-13 15:49:00 +0000

    Created DB using abstractuser.
    in settings, added line AUTH_USER_MODEL = 'SCS.User"
    
    This makes sure that it does not utilise the standard User
    
    these changes should allow us to use Django's built in auth.

--- DESD_2024/SmartCare/SCS/admin.py
+++ DESD_2024/SmartCare/SCS/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile, ContactInfo, Addresse, Appointment, Invoice
+#from .models import UserAuth
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
+#admin.site.register(UserAuth)
+admin.site.register(ContactInfo)
+admin.site.register(Addresse)
+admin.site.register(Appointment)
+admin.site.register(Invoice)
