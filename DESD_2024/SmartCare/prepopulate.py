69dcd31256d5a4b8a2b665f4dc73b8f50a4858ec
Tim <timkchapman@gmail.com>
2024-02-22 10:01:39 +0000

    added missed script for prepopulating data

--- DESD_2024/SmartCare/prepopulate.py
+++ DESD_2024/SmartCare/prepopulate.py
@@ -11,23 +11,57 @@ from SCS.models import *
 def parse_bool(value):
     return value.lower() == 'true'
 
+def open_csv(file):
+    try:
+        file = open(file, 'r')
+        return csv.DictReader(file)
+    except FileNotFoundError:
+        print(f"File {file} not found")
+        return None
+    
+def get_user_by_id(user_id):
+    try:
+        return User.objects.get(userID = user_id)
+    except User.DoesNotExist:
+        print(f"User with ID {user_id} does not exist")
+        return None
+
 def populate_users():
-    with open('data/users.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            user = User.objects.create_user(username = row['username'], 
-                                        password = row['password'], 
-                                        email = row['email'],
-                                        first_name = row['firstname'], 
-                                        last_name = row['lastname'],
-                                        dob = row['date of birth'],
-                                        gender = row['gender'],
-                                        allergies = parse_bool(row['allergies']),
-                                        is_active = parse_bool(row['active']), 
-                                        date_joined = timezone.now())
+    reader = open_csv('data/users.csv')
+    if not reader:
+        return
+    
+    for row in reader:
+        user = User.objects.create_user(username = row['username'], 
+                                    password = row['password'], 
+                                    email = row['email'],
+                                    first_name = row['firstname'], 
+                                    last_name = row['lastname'],
+                                    dob = row['date of birth'],
+                                    gender = row['gender'],
+                                    allergies = parse_bool(row['allergies']),
+                                    is_active = parse_bool(row['active']), 
+                                    date_joined = timezone.now())
 
 def populate_profiles():
-    with open('data/profiles.csv', 'r') as file:
+    reader = open_csv('data/profiles.csv')
+    if not reader:
+        return
+    
+    for row in reader:
+        user_id = int(row['user'])
+        user = get_user_by_id(user_id)
+        if user:
+            profile = Profile.objects.create(user = user,
+                                            isPatient = parse_bool(row['patient']),
+                                            isDoctor = parse_bool(row['doctor']),
+                                            isPartTime = parse_bool(row['part time']),
+                                            isNurse = parse_bool(row['nurse']),
+                                            isAdmin = parse_bool(row['admin']),
+                                            isNHSTrust = parse_bool(row['trust']))
+'''            
+def populate_contact_info():
+    with open('data/contact_info.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             user_id = int(row['user'])
@@ -38,14 +72,40 @@ def populate_profiles():
                 print(f"User with ID {user_id} does not exist")
                 return
             
-            profile = Profile.objects.create(user = user,
-                                            isPatient = parse_bool(row['patient']),
-                                            isDoctor = parse_bool(row['doctor']),
-                                            isPartTime = parse_bool(row['part time']),
-                                            isNurse = parse_bool(row['nurse']),
-                                            isAdmin = parse_bool(row['admin']),
-                                            isNHSTrust = parse_bool(row['trust']))
+            contactInfo = ContactInfo.objects.create(contactType = row['contact type'],
+                                                     contactValue = row['contact value'],
+                                                    description = row['description'],
+                                                    user = user)
+            
+def populate_addresses():
+    with open('data/addresses.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            user_id = int(row['user'])
 
+            try:
+                user = User.objects.get(userID = user_id)
+            except User.DoesNotExist:
+                print(f"User with ID {user_id} does not exist")
+                return
+            
+            address = Address.objects.create(addressID = int(row['addressID']),
+                                            number = int(row['number']),
+                                            buildingName = row['building name'],
+                                            streetName = row['street name'],
+                                            city = row['city'],
+                                            county = row['county'],
+                                            postcode = row['postcode'],
+                                            country = row['country'],
+                                            description = row['description'],
+                                            user = user)
+
+def populate_appointments():
+    with open('data/appointments.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        
+'''
 if __name__ == '__main__':
     print("Starting to populate the database... ")
     populate_users()
