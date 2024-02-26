e61bc6cbf44503c6a66a17c492e38b5382864235
Errn123 <aaron.wearne@gmail.com>
2024-02-26 20:09:24 +0000

    Basic page and url/views changes

--- DESD_2024/SmartCare/SCS/urls.py
+++ DESD_2024/SmartCare/SCS/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import views, login_view
+
 
 
 urlpatterns = [
-   path('', views.index, name='index')
+   path('', views.index, name='index'),
+   path('login/', login_view, name='login'),
 ]
--- DESD_2024/SmartCare/SCS/views.py
+++ DESD_2024/SmartCare/SCS/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+#can change anything here based on our other implementation 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'accounts/login.html')
