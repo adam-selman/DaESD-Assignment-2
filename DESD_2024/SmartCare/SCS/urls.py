db970ddd7c4bed82adc44920f93efc60c655d39a
Sonia <Sonia2.Tadlaoui@live.uwe.ac.uk>
2024-03-04 13:34:37 +0000

    Fixed the login decorator issue, created an auto_logout js script to check for expired sessions to redirect users to the login page then included the script in all dashboards templates . I've also created a bootstrap navbar to access a logout button for manual logouts

--- DESD_2024/SmartCare/SCS/urls.py
+++ DESD_2024/SmartCare/SCS/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
    path('doctor_dashboard', views.doc, name='docDash'),
    path('patient_dashboard', views.patient, name='patDash'),
    path('admin_dashboard', views.admin, name='admDash'),
+   path('logout',views.Logout),
+   path('check_status',views.check_session),
+   path('Session_status',views.Session),
 ]
