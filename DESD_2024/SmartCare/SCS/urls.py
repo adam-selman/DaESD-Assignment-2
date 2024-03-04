a0ff004575ab6cb5e04504a59413bb6251c0e458
Adam Selman <adam.matthew.selman@gmail.com>
2024-03-04 20:57:24 +0000

    adding appointment booking URL

--- DESD_2024/SmartCare/SCS/urls.py
+++ DESD_2024/SmartCare/SCS/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
    path('', views.Auth, name='auth'),
    path('login', views.Login, name='login'),
-    path('nurse_dashboard', views.nurse, name='nursDash'),
+   path('nurse_dashboard', views.nurse, name='nursDash'),
    path('doctor_dashboard', views.doc, name='docDash'),
    path('patient_dashboard', views.patient, name='patDash'),
    path('admin_dashboard', views.admin, name='admDash'),
+   path('patient_appointment_booking', views.patient_appointment_booking, name='patientBooking'),
 ]
