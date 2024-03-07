b933f0038204d41294d4073c994551707244f560
Errn123 <aaron.wearne@gmail.com>
2024-03-07 19:31:21 +0000

    UserRegisterform

--- DESD_2024/SmartCare/SCS/forms.py
+++ DESD_2024/SmartCare/SCS/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from .models import Appointment, UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # class AppointmentBookingForm(forms.Form):
 #     your_name = forms.CharField(label="Your name")
 #     date = forms.DateField(label="Booking Date")
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    user_type = forms.ChoiceField(choices=UserProfile.USER_TYPE_CHOICES)
 
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2", "user_type"]
+
+        
 class AppointmentBookingForm(forms.ModelForm):
     """
     Form object for booking patient appointments
