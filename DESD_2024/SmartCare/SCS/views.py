aeb22eed7e0309c0d3cfe2871c4fd27a62f1487c
Sonia <Sonia2.Tadlaoui@live.uwe.ac.uk>
2024-03-07 01:57:25 +0000

    Changed the login system to only have username and password , added the option to check for empty fields before the POST request is sent , handled the 403 error related to an incorrect token . 4 groups were created via py script and the required decorators have been added to each user's dashboard

--- DESD_2024/SmartCare/SCS/views.py
+++ DESD_2024/SmartCare/SCS/views.py
@@ -4,8 +4,23 @@ from django.http import JsonResponse
 from django.contrib import messages
 #from .forms import LoginForm
 from django.contrib.auth import authenticate, login , logout
-from django.middleware.csrf import get_token
+from django.middleware.csrf import get_token, rotate_token
+from django.middleware import csrf
+from .models import UserProfile
+from django.contrib.auth.decorators import user_passes_test
 
+
+def is_doctor(user):
+    return user.groups.filter(name='doctor_group').exists()
+
+def is_nurse(user):
+    return user.groups.filter(name='nurse_group').exists()
+
+def is_patient(user):
+    return user.groups.filter(name='patient_group').exists()
+
+def is_admin(user):
+    return user.groups.filter(name='admin_group').exists()
 
 def index(request):
     csrf_token = get_token(request)
@@ -21,30 +36,34 @@ def Session(request):
 
 
 def Login(request):
-    csrf_token = get_token(request)
+    csrf_token = csrf.get_token(request)
     check = False
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user_type = request.POST.get('user_type')
+        
 
         user = authenticate(username=username, password=password)
         if user is not None:
-            if user.userprofile.user_type == user_type:
-                login(request, user)
-                if user_type == 'doctor':
-                    return JsonResponse({'success':True,'url': 'doctor_dashboard'})
-                elif user_type == 'patient':
-                    return JsonResponse({'success':True,'url': 'patient_dashboard'})
-                elif user_type == 'nurse':
-                    return JsonResponse({'success':True,'url': 'nurse_dashboard'})
-                elif user_type == 'admin':
-                    return JsonResponse({'success':True,'url': 'admin_dashboard'})
-            else:
-                return HttpResponse("Unauthorized access", status=401)
+            user_profile = UserProfile.objects.get(user=user)
+            user_type = user_profile.user_type
+            
+            login(request, user)
+            #####rotate_token(request)
+            if user_type == 'doctor':
+                return redirect('docDash')
+            elif user_type == 'patient':
+                return redirect('patDash')
+            elif user_type == 'nurse':
+                return redirect('nursDash')
+            elif user_type == 'admin':
+                return redirect('admDash')
+            
+            
         else:
             check = True
             messages.error(request, 'Invalid username or password')
+            
     else:
         check = False
     return render(request, 'login.html',{'csrf_token':csrf_token,'check':check}) 
@@ -52,18 +71,22 @@ def Login(request):
     
 
 @login_required(login_url='login')
+@user_passes_test(is_doctor, login_url='login')
 def doc(request):
     return render(request, 'doctor_dashboard.html')
 
 @login_required(login_url='login')
+@user_passes_test(is_patient, login_url='login')
 def patient(request):
     return render(request, 'patient_dashboard.html')
 
 @login_required(login_url='login')
+@user_passes_test(is_admin, login_url='login')
 def admin(request):
     return render(request, 'admin_dashboard.html')
 
 @login_required(login_url='login')
+@user_passes_test(is_nurse, login_url='login')
 def nurse(request):
     return render(request, 'nurse_dashboard.html')
 
