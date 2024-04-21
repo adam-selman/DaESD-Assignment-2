b6940758a26ee1a2c647a6849fe9c1ff0e862bef
Adam Selman <adam.matthew.selman@gmail.com>
2024-04-21 14:32:29 +0100

    Getting invoices for admin panel

--- DESD_2024/SmartCare/SCS/views.py
+++ DESD_2024/SmartCare/SCS/views.py
@@ -26,7 +26,7 @@ from .models import DoctorProfile, NurseProfile, UserProfile, User, Timetable, S
 
 from .db_utility import get_user_profile_by_user_id, check_practitioner_service, get_invoice_information_by_user_id, \
                     get_medical_services, get_user_profile_by_user_id, get_practitioners_by_day_and_service,  \
-                         make_patient_appointment_booking, get_time_slots_by_day_and_practitioner
+                         make_patient_appointment_booking, get_time_slots_by_day_and_practitioner, get_all_invoice_information
 from .utility import APPOINTMENT_TIMES, parse_times_for_view, calculate_appointment_cost, generate_invoice_file_content
 
 logger = logging.getLogger(__name__)
@@ -358,7 +358,8 @@ def admin(request):
     user_type = "admin"
     user = request.user
     user_name = user.get_full_name
-    return render(request, 'admin_dashboard.html', {'user_type': user_type, "user_name": user_name})
+    invoices = get_all_invoice_information()
+    return render(request, 'admin_dashboard.html', {'user_type': user_type, "user_name": user_name, "invoices": invoices})
 
 @login_required(login_url='login')
 @custom_user_passes_test(is_nurse)
