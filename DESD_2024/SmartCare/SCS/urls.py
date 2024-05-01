6e97a5dba93fa752c6f3c79a27f0ed55cba21197
Sonia <Sonia2.Tadlaoui@live.uwe.ac.uk>
2024-05-01 21:12:22 +0100

    removed name

--- DESD_2024/SmartCare/SCS/urls.py
+++ DESD_2024/SmartCare/SCS/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
    path('login/', views.Login, name='login'),  #fixes for login path includes the needed /
    path('dashboard', views.dashboard_resolver, name='dashboard'),
    path('patient_appointment_booking', views.patient_appointment_booking, name='patientBooking'),
-   path('retrieve_practitioners_by_day_and_service', views.retrieve_practitioners_by_day_and_service, name='getPractitionersByDayAndService'),
-   path('retrieve_time_slots_by_day_and_practitioner', views.retrieve_time_slots_by_day_and_practitioner, name='getPractitionersByDayAndService'),
+   path('retrieve_practitioners_by_day_and_service', views.retrieve_practitioners_by_day_and_service),
+   path('retrieve_time_slots_by_day_and_practitioner', views.retrieve_time_slots_by_day_and_practitioner),
    path('logout',views.Logout),
    path('check_status',views.check_session),
    path('Session_status',views.Session),
